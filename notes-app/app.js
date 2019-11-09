02a7c2012d3c2b2ac83bbc4e688e326c40d19e92
arushkamboj <akamboj@usc.edu>
2019-11-08 18:13:12 -0800

    Added yargs commands template

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,6 +4,40 @@ const yargs = require("yargs");
 
 const getNotes = require("./notes.js");
 
-console.log(yargs.argv);
+// Create Add command
+yargs.command({
+  command: "add",
+  describe: "Add a note",
+  handler: function() {
+    console.log("Adding a note");
+  }
+});
 
-//SSH identity commit
+// Create Remove Command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing a note");
+  }
+});
+
+// Create List Command
+yargs.command({
+  command: "list",
+  describe: "List all the notes",
+  handler: function() {
+    console.log("List of notes here..");
+  }
+});
+
+// Create Read Command
+yargs.command({
+  command: "read",
+  describe: "Read a single note",
+  handler: function() {
+    console.log("Content of note");
+  }
+});
+
+console.log(yargs.argv);
