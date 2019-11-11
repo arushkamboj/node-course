3bd9d74ef3bafc608a645a4f33856f9435c5514b
arushkamboj <akamboj@usc.edu>
2019-11-10 19:56:25 -0800

    Removed Save Notes Implementation

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -22,11 +22,6 @@ const addNote = function(title, body) {
   }
 };
 
-const saveNotes = function(notes) {
-  const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync("notes.json", dataJSON);
-};
-
 const loadNotes = function() {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
