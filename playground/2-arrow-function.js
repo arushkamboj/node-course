ed3f1db918db223edd395308cefd9d67709c6bb8
arushkamboj <akamboj@usc.edu>
2019-11-14 00:04:53 -0800

    Arrow Functions: this binding

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -6,6 +6,22 @@
 //   return x * x;
 // };
 
-const square = x => x * x;
+// const square = x => x * x;
 
-console.log(square(2));
+// console.log(square(2));
+
+/**
+ * Arrow Functions do not bind their own this value
+ * They access the this value in the context they were created
+ */
+
+const event = {
+  name: 'Birthday Party',
+  guestList: ['Arush', 'Neha', 'Himalaya'],
+  printGuestList() {
+    console.log('Guest list for ' + this.name);
+    this.guestList.forEach(guest => {
+      console.log(guest + ' is attending ' + this.name);
+    });
+  }
+};
