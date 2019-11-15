55d19bf6cb83a17ec904ef0659fd1ae0b1a84fb0
arushkamboj <akamboj@usc.edu>
2019-11-14 17:23:21 -0800

    Added Error Handling to request template

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,7 +8,20 @@ request(
     json: true
   },
   (error, response) => {
-    const data = response.body;
-    console.log(data.currently);
+    if (error) {
+      console.log('Unable to connect to the weather service!');
+    } else {
+      const data = response.body;
+      console.log(data.currently);
+    }
   }
 );
+
+// const geoURL =
+//   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJ1c2hrYW1ib2oiLCJhIjoiY2syemZoeHJhMDY0bTNjbDNuZ2NvNnJnbCJ9.FTicrCerGrl7x4vB9TcozQ';
+// request({ url: geoURL, json: true }, (error, response) => {
+//   const latitude = response.body.features[0].center[1];
+//   const longitude = response.body.features[0].center[0];
+
+//   console.log(latitude, longitude);
+// });
