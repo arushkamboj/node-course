9c4356a9203ba556d59d16182ec457e00b5f5878
arushkamboj <akamboj@usc.edu>
2019-11-16 00:24:44 -0800

    Added forecast and geocode utils

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,46 +1,13 @@
 const request = require('request');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url =
-  'https://api.darksky.net/forecast/3693b2a0e64f63986c57be7350a1b5fd/37.8267,-122.4233';
-
-request({ url: url, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to weather service!');
-  } else if (response.body.error) {
-    console.log('Unable to find location');
-  } else {
-    console.log(
-      response.body.daily.data[0].summary +
-        ' It is currently ' +
-        response.body.currently.temperature +
-        ' degress out. There is a ' +
-        response.body.currently.precipProbability +
-        '% chance of rain.'
-    );
-  }
+geocode('Boston', (error, data) => {
+  console.log('error: ' + error);
+  console.log('data: ', data);
 });
 
-const request = require('request');
-
-const geocode = (address, callback) => {
-  const url =
-    'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-    address +
-    '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1';
-
-  request({ url: url, json: true }, (error, response) => {
-    if (error) {
-      callback('Unable to connect to location services!', undefined);
-    } else if (response.body.features.length === 0) {
-      callback('Unable to find location. Try another search.', undefined);
-    } else {
-      callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
-      });
-    }
-  });
-};
-
-module.exports = geocode;
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
+});
