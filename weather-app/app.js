3393d91d7598f9e821ad49c61a25b8601d9ff239
arushkamboj <akamboj@usc.edu>
2019-11-15 23:45:04 -0800

    Added callback abstraction to geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -20,17 +20,27 @@ request({ url: url, json: true }, (error, response) => {
   }
 });
 
-const geocodeURL =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJ1c2hrYW1ib2oiLCJhIjoiY2syemZoeHJhMDY0bTNjbDNuZ2NvNnJnbCJ9.FTicrCerGrl7x4vB9TcozQ';
+const request = require('request');
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to location services!');
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Try another search.');
-  } else {
-    const latitude = response.body.features[0].center[0];
-    const longitude = response.body.features[0].center[1];
-    console.log(latitude, longitude);
-  }
-});
+const geocode = (address, callback) => {
+  const url =
+    'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+    address +
+    '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1';
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to location services!', undefined);
+    } else if (response.body.features.length === 0) {
+      callback('Unable to find location. Try another search.', undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
