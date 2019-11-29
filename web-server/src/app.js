eb80a25cfff56e36e15eefad7e8cd828f500110c
arushkamboj <akamboj@usc.edu>
2019-11-29 01:02:51 -0800

    Added forecast & geocode

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast.js');
+const geocode = require('./utils/geocode.js');
 
 const app = express()
 
@@ -40,10 +42,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        });
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error });
+        }
+
+        forecast(latitude, longitude, (error, _forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+
+            res.send({
+                forecast: _forecastData,
+                location,
+                address: req.query.address
+            });
+        });
+    });
 })
 
 app.get('/help/*', (req, res) => {
