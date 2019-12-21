48a4198a0630a34cb9bbfe96358d97f039fa7a69
arushkamboj <akamboj@usc.edu>
2019-12-21 00:36:06 -0600

    mongoose CRUD

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,20 +1,64 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api'. {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid.');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number.');
+            }
+        }
     }
 });
 
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+// const task = new Tasks({
+//     description: 'This is my first task',
+//     completed: false
+// });
+
+// task.save().then(() => {
+//     console.log(tasks);
+// }).catch((error) => {
+//     console.log('Error', error);
+// });
+
 const me = new User({
     name: 'Arush',
-    age: 27
+    email: 'arush@gamil.com',
+    age: -1
 });
+
+me.save().then(() => {
+    console.log(me);
+}).catch((error) => {
+    console.log('Error!', error);
+});
+
