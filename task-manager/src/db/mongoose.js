871b5619b295171143f315dfa57aa6854f0864f3
arushkamboj <akamboj@usc.edu>
2019-12-22 10:54:41 -0600

    Added Task and User mongoose models

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,64 +1,7 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid.');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number.');
-            }
-        }
-    }
-});
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
-
-// const task = new Tasks({
-//     description: 'This is my first task',
-//     completed: false
-// });
-
-// task.save().then(() => {
-//     console.log(tasks);
-// }).catch((error) => {
-//     console.log('Error', error);
-// });
-
-const me = new User({
-    name: 'Arush',
-    email: 'arush@gamil.com',
-    age: -1
-});
-
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!', error);
-});
-
