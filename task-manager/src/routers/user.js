57c6f15261fa7b00f88caa7e2040ab8cfcd7fdb4
arushkamboj <akamboj@usc.edu>
2019-12-29 12:05:10 -0600

    Added authentication routes and middleware

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middlerware/auth');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -24,13 +25,31 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const users = await User.find({});
-        res.send(users)
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+        res.send();
     } catch (e) {
         res.status(500).send();
     }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.users.tokens = [];
+        await req.users.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
